d9d464e3ab4fdc284a2a180861c57c135d1a8415
Yogesh <you@example.com>
2020-12-07 15:11:54 +0530

    Tour Setup

---
+++ src/Tours.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Tour from './Tour';
+const Tours = ({tours,removeItems}) => {
+  return(
+      <section>
+          <div className='title'>
+            <h1>Our Tours</h1>
+            <div className="underline"></div>
+          </div>
+          <div>
+            {tours.map((tour)=>{
+                return <Tour key={tour.id} {...tour} removeItems={removeItems}/>
+            })}
+          </div>
+      </section>
+  );
+};
+
+export default Tours;
